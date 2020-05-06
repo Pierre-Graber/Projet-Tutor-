53abdce6e3e7a2a6cf615fe1e6ae501698582e5d
pierre.grabz@gmail.com <pgrabe910e@emi.u-bordeaux1.fr>
2020-05-06 18:07:35 +0200

    6/05/2020

---
+++ Sage/generateur.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+import time
+import matplotlib
+import matplotlib.pyplot as plt
+import hashlib
+from random_search import RS
+
+def TestGen(n,k,gen):
+    " Test n fois la génération de nombres premiers pouvant être codés sur k-bits, renvoie un couple : temps moyen d'exécution , bugs "
+    l = []
+    tps=0
+    bug = 0
+    for i in range (0,n):
+        start = time.time()
+        try :
+            prime = gen(k)
+        except :
+            bug +=1
+        end = time.time()
+        tps += (end-start)
+
+    return (tps/n,bug)
+
+
+
+def TestRS(n,k,B):
+    l = []
+    tps=0
+    bug = 0
+    for i in range (0,n):
+        start = time.time()
+        try :
+            prime = RS(k,is_pseudoprime,B)
+            
+        except :
+            bug +=1
+
+        end = time.time()
+        tps += (end-start)
+
+    return (tps/n,bug)
+    
+
+
+def gen_courbesRS() :
+    x= []
+    y=[]
+    for i in range(6,20):
+        y.append(TestRS(2000,1000,2**i)[0])
+        x.append(i)
+
+    return x,y
---
+++ Sage/trace.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+def trace(gen1,gen2):
+    n=gen1[1]
+    plt.plot(n,gen1[0],"r--",label="RS")
+    plt.plot(n,gen2[0],label='G')
+    plt.xlabel("taille en bits de p et q")
+    plt.ylabel("vitesse de factorisation en sec")
+    plt.legend()
+    plt.show()
+    
+    
+def traceMaurer(gen1,gen2):
+    n=gen1[1]
+    plt.plot(n,gen1[0],"r--",label="TrialDivision")
+    plt.plot(n,gen2[0],label='is_pseudoprime')
+    plt.xlabel("taille en bits")
+    plt.ylabel("vitesse de génération en sec")
+    plt.legend()
+    plt.show()
