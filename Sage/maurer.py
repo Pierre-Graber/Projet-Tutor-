6b209efa07df3b8d74463880e238ef633c40892d
pierre.grabz@gmail.com <pgrabe910e@emi.u-bordeaux1.fr>
2020-05-06 18:08:07 +0200

    6/05/2020

--- Sage/maurer.py
+++ Sage/maurer.py
@@ -1,12 +1,13 @@
 from primality_tests import*
+from tools import*
 
-def maurer(k):
+def maurer(k,test=TrialDivision):
     b1 = True
     b2 = True
     if k <= 20 :
         while b1:
             n = Integer(randint(0,2**k))
-            if TrialDivision(n):
+            if test(n):
                 b1 = False
     else :
         while b2:
@@ -31,7 +32,7 @@ def maurer(k):
                 R = randint(I+1, 2*I)
                 
                 n=2*R*q+1
-                if TrialDivision(n):
+                if test(n):
                     a = randint(2,n-1)
                     b = power_mod(a,n-1,n)
                     if b==1 :
@@ -42,3 +43,43 @@ def maurer(k):
                             b2=False
                             
     return n
+
+#======================================================================================================================
+
+def TestMaurer(n,k,test):
+    " Test n fois la génération de nombres premiers pouvant être codés sur k-bits, renvoie un couple : temps moyen d'exécution , bugs "
+    l = []
+    tps=0
+    bug = 0
+    for i in range (0,n):
+        start = time.time()
+        prime=maurer(k,test)
+        end = time.time()
+        tps += (end-start)
+
+    return (tps/n)
+
+def gen_courbesMaurer() :
+    x1=[]
+    x2=[] 
+    y=[]
+    for i in ListeBits:
+        x1.append(TestMaurer(600,i,TrialDivision))
+        x2.append(TestMaurer(600,i,is_pseudoprime))
+        y.append(i)
+	print(y)
+
+    return (x1,y),(x2,y)
+
+
+def traceMaurer(gen1,gen2):
+    n=gen1[1]
+    plt.plot(n,gen1[0],"r--",label="TrialDivision")
+    plt.plot(n,gen2[0],label='is_pseudoprime')
+    plt.xlabel("taille en bits")
+    plt.ylabel("vitesse de génération en sec")
+    plt.legend()
+    plt.show()
+
+
+
